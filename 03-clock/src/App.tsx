6ab37100f1d7623f2df9635d4ed60538b394bcf6
Johan Nordström <johan@digitalvillage.se>
2024-05-03 12:20:01 +0200

    🕰️ Clock skeleton.

---
+++ 03-clock/src/App.tsx
@@ -0,0 +1,13 @@
+import "./assets/scss/App.scss";
+
+function App() {
+	return (
+		<div className="container">
+			<div className="display-1 text-center">
+				00:13:37
+			</div>
+		</div>
+	);
+}
+
+export default App;
