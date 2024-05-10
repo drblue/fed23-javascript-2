20078b0e9a870bea4eb781bd4e194e3d649d1588
Johan Nordström <johan@digitalvillage.se>
2024-05-10 12:51:30 +0200

    Added TodoPage skeleton and workshop instructions.

--- 06-better-todos/src/pages/TodoPage.tsx
+++ 06-better-todos/src/pages/TodoPage.tsx
@@ -4,7 +4,19 @@ const TodoPage = () => {
 	const { id } = useParams();
 
 	return (
-		<div>Todo ID {id}</div>
+		<>
+			<h1>TITLE for todo with id {id}</h1>
+
+			<p><strong>Status:</strong> COMPLETED|NOT COMPLETED</p>
+
+			<div className="buttons mb-3">
+				{/* Toggle */}
+
+				{/* Delete */}
+			</div>
+
+			{/* Here be button-link back to all todos */}
+		</>
 	)
 }
 
