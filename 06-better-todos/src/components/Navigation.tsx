f47231d79b6e9c55de0b5916b03dcf5f96fc4201
Johan Nordström <johan@digitalvillage.se>
2024-05-10 11:54:13 +0200

    Replaced ordinary, boring links with React Router Links 🤓.

--- 06-better-todos/src/components/Navigation.tsx
+++ 06-better-todos/src/components/Navigation.tsx
@@ -1,18 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
-				<Navbar.Brand href="/">📝 Better Todos</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">📝 Better Todos</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/todos">Todos</Nav.Link>
+						<Nav.Link as={Link} to="/">Home</Nav.Link>
+						<Nav.Link as={Link} to="/todos">Todos</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
--- 06-better-todos/src/pages/HomePage.tsx
+++ 06-better-todos/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
 	return (
 		<>
 			<h1>Welcome to Better Todos!</h1>
 
-			<p>Because when your life is on fire 🔥, you need a <a href="/todos">todo list</a>.</p>
+			<p>Because when your life is on fire 🔥, you need a <Link to="/todos">todo list</Link>.</p>
 		</>
 	)
 }
