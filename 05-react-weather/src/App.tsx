2e876b113bf2738566e8d4e871c9e9f001f5bd09
Johan Nordström <johan@digitalvillage.se>
2024-05-10 09:24:16 +0200

    React Weather - Show loading animation.

--- 05-react-weather/src/App.tsx
+++ 05-react-weather/src/App.tsx
@@ -3,24 +3,29 @@ import Forecast from "./components/Forecast";
 import SearchCity from "./components/SearchCity";
 import { getCurrentWeather } from "./services/OWMAPI";
 import { WeatherReport } from "./services/OWMAPI.types";
+import Airplane from "./assets/images/747.svg";
 import "./assets/scss/App.scss";
 
 function App() {
 	const [currentWeather, setCurrentWeather] = useState<WeatherReport | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleSearch = async (location: string) => {
-		console.log("Someone wants the weather for:", location);
+		setIsLoading(true);
 
 		// Call API and ask for weather in `location`
 		const data = await getCurrentWeather(location);
 
 		// Update current weather state with the weather in `location`
 		setCurrentWeather(data);
+		setIsLoading(false);
 	}
 
 	return (
 		<div id="app" className="container">
 			<SearchCity onSearch={handleSearch} />
+
+			{isLoading && <img src={Airplane} className="img-fluid py-5 w-100" />}
 
 			{currentWeather && <Forecast data={currentWeather} />}
 		</div>
