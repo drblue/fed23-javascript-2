54351c412b6baa17a9b3baeb7a341cd3f9694214
Johan Nordström <johan@digitalvillage.se>
2024-05-15 10:56:12 +0200

    Hacker News: Took API service to a whole other level by utilising Generic Functions 🤯🤓🤩😎🥳.

--- 07-hackernews/src/services/HackerNewsAPI.ts
+++ 07-hackernews/src/services/HackerNewsAPI.ts
@@ -18,14 +18,23 @@ const instance = axios.create({
 });
 
 /**
+ * Execute a HTTP GET request to an endpoint
+ *
+ * @param endpoint Endpoint to GET
+ */
+const get = async <T>(endpoint: string) => {
+	const res = await instance.get<T>(endpoint);
+	return res.data;
+}
+
+/**
  * Search Hacker News stories
  *
  * @param query Query to search for
  * @param page Page of search result to get
  */
-export const search = async (query: string, page = 0) => {
-	const res = await instance.get<HN_SearchResponse>(`/search?query=${query}&tags=story&page=${page}`);
-	return res.data;
+export const search = (query: string, page = 0) => {
+	return get(`/search?query=${query}&tags=story&page=${page}`);
 }
 
 /**
@@ -34,7 +43,14 @@ export const search = async (query: string, page = 0) => {
  * @param query Query to search for
  * @param page Page of search result to get
  */
-export const searchByDate = async (query: string, page = 0) => {
-	const res = await instance.get<HN_SearchResponse>(`/search_by_date?query=${query}&tags=story&page=${page}`);
-	return res.data;
+export const searchByDate = (query: string, page = 0) => {
+	return get<HN_SearchResponse>(`/search_by_date?query=${query}&tags=story&page=${page}`);
 }
+
+/*
+// Just an example to demo sending a type argument to a generic function
+// Not anything that exist in the API
+export const getStoryById = (story_id: number) => {
+	return get<HN_SearchHit>(`/stories/${story_id}`);
+}
+*/
