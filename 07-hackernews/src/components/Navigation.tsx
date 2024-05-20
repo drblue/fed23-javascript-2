5e74e7f249c8edbfcece723017d00a5d74e2feb6
Johan Nordström <johan@digitalvillage.se>
2024-05-20 11:41:10 +0200

    Added theme switcher to navigation menu.

--- 07-hackernews/src/components/Navigation.tsx
+++ 07-hackernews/src/components/Navigation.tsx
@@ -1,9 +1,20 @@
+import { useContext } from "react";
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link, NavLink } from "react-router-dom";
+import { ThemeContext } from "../contexts/ThemeContextProvider";
 
 const Navigation = () => {
+	const themeContext = useContext(ThemeContext);
+	if (!themeContext) {
+		throw new Error("Trying to use ThemeContext outside of ThemeContextProvider");
+	}
+
+	const { isDarkMode, toggleTheme } = themeContext;
+
+
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
 			<Container>
@@ -15,6 +26,10 @@ const Navigation = () => {
 						<Nav.Link as={NavLink} end to="/search">Search</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
+
+				<Button variant="outline-secondary" onClick={toggleTheme}>
+					{isDarkMode ? "☀️" : "🌙"}
+				</Button>
 			</Container>
 		</Navbar>
 	)
