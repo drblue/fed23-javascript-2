776e1e78728ebe57f736b3a3cdb4b1a728553b6b
Johan Nordström <johan@digitalvillage.se>
2024-05-22 10:39:05 +0200

    Added `changeUrl` so we don't have to expose `setUrl`. Also validate the URL being set. Handle errors with both a `isError` and `error` state.

--- 07-hackernews/src/hooks/useGetRandomDogImage.ts
+++ 07-hackernews/src/hooks/useGetRandomDogImage.ts
@@ -4,20 +4,40 @@ import { RandomDogImage } from "../types/DogAPI.types";
 
 const useGetRandomDogImage = () => {
 	const [data, setData] = useState<RandomDogImage | null>(null);
+	const [error, setError] = useState<string | null>(null);
+	const [isError, setIsError] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
 	const [url, setUrl] = useState<string | null>(null);
+
+	const changeUrl = (newUrl: string) => {
+		// Validate that `newUrl` actually is a valid URL
+		try {
+			const validatedUrl = new URL(newUrl);
+			setUrl(validatedUrl.toString());
+		} catch (err) {
+			setError(err instanceof Error ? err.message : "Something that was not an Error was thrown");
+			setIsError(true);
+		}
+	}
 
 	const getData = async (resourceUrl: string) => {
 		// reset state
 		setData(null);
+		setIsError(false);
 		setIsLoading(true);
 
 		// get data from api
-		const res = await axios.get<RandomDogImage>(resourceUrl);
-		await new Promise((r) => setTimeout(r, 1500));
+		try {
+			const res = await axios.get<RandomDogImage>(resourceUrl);
+			await new Promise((r) => setTimeout(r, 1500));
 
-		// update state with data
-		setData(res.data);
+			// update state with data
+			setData(res.data);
+		} catch (err) {
+			setError(err instanceof Error ? err.message : "Something that was not an Error was thrown");
+			setIsError(true);
+		}
+
 		setIsLoading(false);
 	};
 
@@ -36,10 +56,12 @@ const useGetRandomDogImage = () => {
 	}, [execute]);
 
 	return {
+		changeUrl,
 		data,
+		error,
 		execute,
+		isError,
 		isLoading,
-		setUrl,
 	}
 }
 
--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -1,9 +1,10 @@
+import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import useGetRandomDogImage from "../hooks/useGetRandomDogImage";
 
 const RandomDogPage = () => {
-	const { data, execute, isLoading, setUrl } = useGetRandomDogImage();
+	const { changeUrl, data, error, execute, isError, isLoading } = useGetRandomDogImage();
 
 	return (
 		<>
@@ -11,12 +12,12 @@ const RandomDogPage = () => {
 
 			<div className="mb-3">
 				<Button
-					onClick={() => setUrl("https://dog.ceo/api/breeds/image/random")}
+					onClick={() => changeUrl("https://dog.ceo/api/breeds/image/random")}
 				>Random doggo</Button>
 
 				<Button
 					className="ms-1"
-					onClick={() => setUrl("https://dog.ceo/api/breed/shiba/images/random")}
+					onClick={() => changeUrl("https://dog.ceo/api/breed/shiba/images/random")}
 				>Random Shiba fluffer</Button>
 
 				<Button
@@ -27,9 +28,17 @@ const RandomDogPage = () => {
 				<Button
 					className="ms-1"
 					variant="warning"
-					onClick={() => {}}
+					onClick={() => changeUrl("passwords.txt")}
 				>Make things go 💣</Button>
+
+				<Button
+					className="ms-1"
+					variant="warning"
+					onClick={() => changeUrl("http://löxmidnvt8drycintwrensweiuym.com")}
+				>Break stuff ⛓️‍💥</Button>
 			</div>
+
+			{isError && <Alert variant="warning">{error}</Alert>}
 
 			{isLoading && <p>Loading...</p>}
 
