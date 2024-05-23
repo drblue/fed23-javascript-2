e977953164a9fe4695aae0c250908b2129ff1386
Johan Nordström <johan@digitalvillage.se>
2024-05-23 09:34:36 +0200

    Set initial type of random dog image that should be fetched (if any).

--- 07-hackernews/src/hooks/useGetRandomDogImage.ts
+++ 07-hackernews/src/hooks/useGetRandomDogImage.ts
@@ -1,8 +1,12 @@
 import { RandomDogImage } from "../types/DogAPI.types";
 import useGetData from "./useGetData";
 
-const useGetRandomDogImage = () => {
-	return useGetData<RandomDogImage>("https://dog.ceo/api/breed/mountain/bernese/images/random");
+const useGetRandomDogImage = (breed?: string) => {
+	const url = breed
+		? `https://dog.ceo/api/breed/${breed}/images/random`
+		: "https://dog.ceo/api/breeds/image/random";
+
+	return useGetData<RandomDogImage>(url);
 }
 
 export default useGetRandomDogImage;
--- 07-hackernews/src/pages/RandomDogPage.tsx
+++ 07-hackernews/src/pages/RandomDogPage.tsx
@@ -4,7 +4,7 @@ import Image from "react-bootstrap/Image";
 import useGetRandomDogImage from "../hooks/useGetRandomDogImage";
 
 const RandomDogPage = () => {
-	const { changeUrl, data, error, execute, isError, isLoading } = useGetRandomDogImage();
+	const { changeUrl, data, error, execute, isError, isLoading } = useGetRandomDogImage("boxer");
 
 	return (
 		<>
