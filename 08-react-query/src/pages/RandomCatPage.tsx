09766a728c2b7a39c13a1e4ab54bfa39ebcaee18
Johan Nordström <johan@digitalvillage.se>
2024-05-29 11:36:39 +0200

    Override query options per query.

--- 08-react-query/src/pages/RandomCatPage.tsx
+++ 08-react-query/src/pages/RandomCatPage.tsx
@@ -19,6 +19,8 @@ const RandomCatPage = () => {
 	} = useQuery({
 		queryKey: ["random-cat", breed],
 		queryFn: () => getRandomCatImage(breed),
+		staleTime: 1000 * 45,  // 45 seconds
+		gcTime: Infinity,  // ✋🏻🗑️🐱🚚
 	});
 
 	return (
