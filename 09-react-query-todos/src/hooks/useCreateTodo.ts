f9ed94c41bc9027fed6aa751a04635545489aca9
Johan Nordström <johan@digitalvillage.se>
2024-06-05 15:02:23 +0200

    Added useCreateTodo custom hook.

---
+++ 09-react-query-todos/src/hooks/useCreateTodo.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createTodo, getTodos } from "../services/TodosAPI";
+import { Todo } from "../services/TodosAPI.types";
+
+const useCreateTodo = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: createTodo,
+		onSuccess: async (newTodo) => {
+			// get ["todos"] from the cache if it exists and is fresh,
+			// otherwise fetch it from the API
+			const cachedTodos = await queryClient.fetchQuery({
+				queryKey: ["todos"],
+				queryFn: getTodos,
+			});
+
+			// instead of invalidating the ["todos"] query, we can construct new data
+			// based on the previous data + newly created todo from the create Todo request
+			// **ONLY** append the new todo if it's not already in the cache
+			if (!cachedTodos.find(todo => todo.id === newTodo.id)) {
+				queryClient.setQueryData<Todo[]>(["todos"], (oldTodos) => {
+					return [
+						...oldTodos ?? [],
+						newTodo,
+					];
+				});
+			}
+
+			// also insert the new todo into the query cache
+			queryClient.setQueryData(["todo", { id: newTodo.id }], newTodo);
+		}
+	})
+};
+
+export default useCreateTodo;
--- 09-react-query-todos/src/pages/CreateTodoPage.tsx
+++ 09-react-query-todos/src/pages/CreateTodoPage.tsx
@@ -1,39 +1,10 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Alert from "react-bootstrap/Alert";
 import { Link } from "react-router-dom";
 import AddNewTodoForm from "../components/AddNewTodoForm"
-import { createTodo, getTodos } from "../services/TodosAPI";
-import { Todo } from "../services/TodosAPI.types";
+import useCreateTodo from "../hooks/useCreateTodo";
 
 const CreateTodoPage = () => {
-	const queryClient = useQueryClient();
-
-	const createTodoMutation = useMutation({
-		mutationFn: createTodo,
-		onSuccess: async (newTodo) => {
-			// get ["todos"] from the cache if it exists and is fresh,
-			// otherwise fetch it from the API
-			const cachedTodos = await queryClient.fetchQuery({
-				queryKey: ["todos"],
-				queryFn: getTodos,
-			});
-
-			// instead of invalidating the ["todos"] query, we can construct new data
-			// based on the previous data + newly created todo from the create Todo request
-			// **ONLY** append the new todo if it's not already in the cache
-			if (!cachedTodos.find(todo => todo.id === newTodo.id)) {
-				queryClient.setQueryData<Todo[]>(["todos"], (oldTodos) => {
-					return [
-						...oldTodos ?? [],
-						newTodo,
-					];
-				});
-			}
-
-			// also insert the new todo into the query cache
-			queryClient.setQueryData(["todo", { id: newTodo.id }], newTodo);
-		}
-	});
+	const createTodoMutation = useCreateTodo();
 
 	return (
 		<>
