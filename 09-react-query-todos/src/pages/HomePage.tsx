e19ca0af0bbd20de51bf731cee49b7ce6545d0b2
Johan Nordström <johan@digitalvillage.se>
2024-06-07 10:18:09 +0200

    So many toasts! 🍞

--- 09-react-query-todos/src/pages/HomePage.tsx
+++ 09-react-query-todos/src/pages/HomePage.tsx
@@ -15,6 +15,26 @@ const HomePage = () => {
 					onClick={() => toast("Wow 🤩! Such click 🐭, much toast 🍞, very celebrate 🥂!")}
 					variant="primary"
 				>Celebrate 🎉</Button>
+
+				<Button
+					onClick={() => toast.success("Wow, such success, very influencer, much money! 💰")}
+					variant="success"
+				>Click me 🤑</Button>
+
+				<Button
+					onClick={() => toast.warn("Wow, such WARNING, very ALERT!")}
+					variant="warning"
+				>Call da police 👮🏻</Button>
+
+				<Button
+					onClick={() => toast.error("Wow, such ERROR, very DANGEROUS!")}
+					variant="danger"
+				>Blow shit up 💣</Button>
+
+				<Button
+					onClick={() => toast.info("LIKE && SUBSCRIBE")}
+					variant="info"
+				>Booooring 🥱</Button>
 			</ButtonGroup>
 		</>
 	)
