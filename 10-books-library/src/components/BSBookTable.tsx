0fcee662541f34b8a06080dffc87211116529a8b
Johan Nordström <johan@digitalvillage.se>
2024-06-10 11:24:05 +0200

    Added smol workshop for sorting data.

--- 10-books-library/src/components/BSBookTable.tsx
+++ 10-books-library/src/components/BSBookTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Table from "react-bootstrap/Table";
 import { Link } from "react-router-dom";
 import { Book } from "../services/BooksAPI.types";
@@ -6,19 +7,37 @@ interface BSBookTableProps {
 	books: Book[];
 }
 
+// type SortKeys = "title" | "author" | "pages" | "published";
+type SortKeys = keyof Book; // get a literal string union of all keys in Book type
+
 const BSBookTable: React.FC<BSBookTableProps> = ({ books }) => {
+	const [sortKey, setSortKey] = useState<SortKeys | null>(null);
+	const [sortAscending, setSortAscending] = useState(true);
+
 	if (!books.length) {
 		return <p>No books for you!</p>;
+	}
+
+	/**
+	 * Sort data by key
+	 *
+	 * @param key Key to sort by
+	 */
+	const sortBy = (key: SortKeys) => {
+		// 1. If we don't already sort by this key, sort by this key
+		// 2. If we already sort by this key, reverse the order
+		// 3. If we already sort by this key and in reverse order, don't sort
+		// 4. Sort numeric data correctly
 	}
 
 	return (
 		<Table bordered hover responsive striped>
 			<thead>
 				<tr>
-					<th>Title</th>
+					<th onClick={() => sortBy("title")}>Title</th>
 					<th>Author</th>
-					<th>Pages</th>
-					<th>Published</th>
+					<th onClick={() => sortBy("pages")}>Pages</th>
+					<th onClick={() => sortBy("published")}>Published</th>
 				</tr>
 			</thead>
 
