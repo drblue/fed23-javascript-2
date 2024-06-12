f644746734f59424aa3aaad5fd3066adfe2219b0
Johan Nordström <johan@digitalvillage.se>
2024-06-12 09:33:56 +0200

    Authors Table using TanStack Basic Table and createColumnHelper.

--- 10-books-library/src/pages/AuthorsPage.tsx
+++ 10-books-library/src/pages/AuthorsPage.tsx
@@ -1,6 +1,39 @@
+import { createColumnHelper } from "@tanstack/react-table";
 import WarningAlert from "../components/alerts/WarningAlert";
-import BSAuthorTable from "../components/BSAuthorTable";
+import TanstackBasicTable from "../components/TanstackBasicTable";
 import useAuthors from "../hooks/useAuthors";
+import { Author } from "../services/BooksAPI.types";
+
+/*
+const columns: ColumnDef<Author>[] = [
+	{
+		accessorKey: "name",
+		header: "Name",
+	},
+	{
+		accessorKey: "date_of_birth",
+		header: "Date of birth",
+		meta: {
+			align: "end",
+		},
+	},
+];
+*/
+
+const columnHelper = createColumnHelper<Author>();
+
+const columns = [
+	columnHelper.accessor("name", {
+		header: "Name",
+	}),
+	columnHelper.accessor("date_of_birth", {
+		header: "Date of birth",
+		meta: {
+			align: "end",
+		},
+	}),
+];
+
 
 const AuthorsPage = () => {
 	const { data: authors, isError, isLoading } = useAuthors();
@@ -17,7 +50,7 @@ const AuthorsPage = () => {
 
 			{isLoading && <p>Loading authors...</p>}
 
-			{authors && <BSAuthorTable authors={authors} />}
+			{authors && <TanstackBasicTable columns={columns} data={authors} />}
 		</>
 	);
 };
