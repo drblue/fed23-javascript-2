6fce67a9bbc938d41fd2d4bf6a1d0367ea928068
Johan Nordström <johan@digitalvillage.se>
2024-06-12 13:40:03 +0200

    Added React Hook Form to `CreateAuthorForm` (but no validation yet).

--- 10-books-library/src/components/forms/CreateAuthorForm.tsx
+++ 10-books-library/src/components/forms/CreateAuthorForm.tsx
@@ -1,17 +1,25 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import useCreateAuthor from "../../hooks/useCreateAuthor";
+import { NewAuthor } from "../../services/BooksAPI.types";
 
 const CreateAuthorForm = () => {
+	const { handleSubmit, register } = useForm<NewAuthor>();
+
+	const onCreateAuthorSubmit: SubmitHandler<NewAuthor> = (data) => {
+		console.log("Submitted data:", data);
+	}
+
 	return (
-		<Form>
+		<Form onSubmit={handleSubmit(onCreateAuthorSubmit)}>
 			<Form.Group className="mb-3" controlId="name">
 				<Form.Label>Author Name</Form.Label>
 				<Form.Control
 					placeholder="Astrid Lindgren"
-					required
 					type="text"
+					{...register("name")}
 				/>
 			</Form.Group>
 
@@ -19,6 +27,7 @@ const CreateAuthorForm = () => {
 				<Form.Label>Date of Birth</Form.Label>
 				<Form.Control
 					type="date"
+					{...register("date_of_birth")}
 				/>
 			</Form.Group>
 
