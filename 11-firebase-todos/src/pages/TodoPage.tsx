58ddf535e1e28efdf476f5c4eeacec277ea6e639
Johan Nordström <johan@digitalvillage.se>
2024-06-17 15:24:28 +0200

    Get a single document from Firestore 🔥🔥.

--- 11-firebase-todos/src/pages/TodoPage.tsx
+++ 11-firebase-todos/src/pages/TodoPage.tsx
@@ -1,22 +1,62 @@
-import { useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Link, useParams } from "react-router-dom";
 import ConfirmationModal from "../components/ConfirmationModal";
 import AutoDismissingAlert from "../components/AutoDismissingAlert";
 import useStatusLocation from "../hooks/useStatusLocation";
 import { Todo } from "../types/Todo.types";
-
-const todo: Todo = {
-	_id: "Xi6VigUyelerlFbSHKTZ",
-	title: "Learn to fake better data 😅",
-	completed: true,
-};
+import { todosCol } from "../services/firebase";
 
 const TodoPage = () => {
+	const [todo, setTodo] = useState<Todo | null>(null);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const [showDeleteModal, setShowDeleteModal] = useState(false);
 	const { id } = useParams();
-	const todoId = Number(id);
 	const location = useStatusLocation();
+
+	const getData = async (documentId: string) => {
+		setError(false);
+		setLoading(true);
+		setTodo(null);
+
+		// Get reference to document in "todos" collection
+		const docRef = doc(todosCol, documentId);
+		const docSnapshot = await getDoc(docRef);
+
+		if (!docSnapshot.exists()) {
+			setTodo(null);
+			setError(true);
+			setLoading(false);
+			return;
+		}
+
+		const data = {
+			...docSnapshot.data(),
+			_id: docSnapshot.id,
+		}
+
+		setTodo(data);
+		setLoading(false);
+	}
+
+	// Get todo on component mount
+	useEffect(() => {
+		if (!id) {
+			return;
+		}
+
+		getData(id);
+	}, [id]);
+
+	if (error) {
+		return <p>Ooops, bad stuff happend. Try again later?</p>
+	}
+
+	if (loading || !todo) {
+		return <p>Loading...</p>
+	}
 
 	return (
 		<>
@@ -42,7 +82,7 @@ const TodoPage = () => {
 					Toggle
 				</Button>
 
-				<Link to={`/todos/${todoId}/edit`} className="btn btn-warning" role="button">
+				<Link to={`/todos/${id}/edit`} className="btn btn-warning" role="button">
 					Edit
 				</Link>
 
@@ -53,7 +93,7 @@ const TodoPage = () => {
 
 			<ConfirmationModal
 				onCancel={() => setShowDeleteModal(false)}
-				onConfirm={() => console.log("Would delete todo with id:", todoId)}
+				onConfirm={() => console.log("Would delete todo with id:", id)}
 				show={showDeleteModal}
 				title="Confirm delete"
 				variant="danger"
